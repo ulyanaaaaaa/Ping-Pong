70491c91111bb0637e3548c085fa5b667b1351ed
Ulyana <ulianabober@gmail.com>
2024-10-08 12:44:36 +0300

    fix(<Ball>)

--- PingPong2D/Assets/Source/Scripts/Ball.cs
+++ PingPong2D/Assets/Source/Scripts/Ball.cs
@@ -12,11 +12,7 @@ public class Ball : MonoBehaviour
     private void Awake()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
-    }
-
-    private void Start()
-    {
-        _direction = new Vector2(Random.Range(-1f,1f), Random.Range(-1f,1f));
+        _direction = new Vector2(Random.Range(0f,1f), Random.Range(-1f,1f));
     }
 
     private void Update()
@@ -29,11 +25,13 @@ public class Ball : MonoBehaviour
         if (collision.gameObject.TryGetComponent(out Platform platform))
         {
             _direction.y = -_direction.y;
+            _direction.x += Random.Range(-0.5f, 0.5f);
         }
         
         if (collision.gameObject.TryGetComponent(out Block block))
         {
             _direction.y = -_direction.y;
+            _direction.x += Random.Range(-0.5f, 0.5f);
         }
 
         if (collision.gameObject.TryGetComponent(out Defence defence))
--- PingPong2D/Assets/Source/Scripts/WindowManager.cs
+++ PingPong2D/Assets/Source/Scripts/WindowManager.cs
@@ -1,38 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(BlockManager))]
 public class WindowManager : MonoBehaviour
 {
-   [SerializeField] private FailTrigger _failTrigger;
-   private BlockManager _blockManager;
+    [SerializeField] private FailTrigger _failTrigger;
+    private BlockManager _blockManager;
 
-   [SerializeField] private GameObject _winPanel;
-   [SerializeField] private GameObject _failPanel;
+    [SerializeField] private GameObject _winPanel;
+    [SerializeField] private GameObject _failPanel;
    
-   private void Awake()
-   {
-      _blockManager = GetComponent<BlockManager>();
-   }
+    private void Awake()
+    {
+        _blockManager = GetComponent<BlockManager>();
+    }
 
-   private void OnEnable()
-   {
-      _failTrigger.OnFail += Fail;
-      _blockManager.OnWin += Win;
-   }
+    private void OnEnable()
+    {
+        _failTrigger.OnFail += Fail;
+        _blockManager.OnWin += Win;
+    }
 
-   private void OnDisable()
-   {
-      _failTrigger.OnFail -= Fail;
-      _blockManager.OnWin -= Win;
-   }
+    private void OnDisable()
+    {
+        _failTrigger.OnFail -= Fail;
+        _blockManager.OnWin -= Win;
+    }
 
-   private void Win()
-   {
-      _winPanel.SetActive(true);
-   }
+    private void Win()
+    {
+        _winPanel.SetActive(true);
+    }
 
-   private void Fail()
-   {
-      _failPanel.SetActive(true);
-   }
+    private void Fail()
+    {
+        _failPanel.SetActive(true);
+    }
 }
